525b8b90d6bbdb77a8cd8164b976bbf9aee2daa0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-09 18:58:19 +0530

    Event Handlers:
    onWeightChange and onHeightChange functions are defined to update the weight and height state variables respectively when the corresponding range inputs are changed by the user.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const App = () => {
   const [height, setHeight] = useState(180);
   const [weight, setWeight] = useState(70);
+  
+  function onWeightChange(event) {
+    setWeight(event.target.value);
+  }
 
+  function onHeightChange(event) {
+    setHeight(event.target.value);
+  }
+  
   
   return (
     <div>
