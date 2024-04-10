027e103f3ccd4b1c8ca71e31e3bc0e8a5fac46b9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-10 22:33:30 +0530

    Functional Component Declaration:
    The Cart component is declared as a functional component using arrow function syntax.
    It takes three props as input: cart, setCart, and handleChange.

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Cart = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Cart = ({ cart, setCart, handleChange }) => {
+    const [price, setPrice] = useState(0);
+    const [totalItems, setTotalItems] = useState(0);
+    
+    return (
+        <div>
+        
+        </div>
+    )
 }
 
 export default Cart
