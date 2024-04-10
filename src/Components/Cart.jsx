ae72b909335ab5a2d90f2894b675fef2c819422b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-10 22:34:56 +0530

    Handle Remove Function:
    handleRemove is a function that removes an item from the cart based on its id.
    It filters the cart array to exclude the item with the specified id and updates the cart state using setCart.

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -4,6 +4,10 @@ const Cart = ({ cart, setCart, handleChange }) => {
     const [price, setPrice] = useState(0);
     const [totalItems, setTotalItems] = useState(0);
 
+    const handleRemove = (id) => {
+        const updatedCart = cart.filter((item) => item.id !== id);
+        setCart(updatedCart);
+    };
     
     return (
         <div>
