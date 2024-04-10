d58e6e36730d5c502b4321b0f8fe312e8e76e84e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-10 22:34:29 +0530

    State Initialization:
    Two state variables, price and totalItems, are initialized using the useState hook.
    price state is used to keep track of the total price of items in the cart.
    totalItems state is used to keep track of the total number of items in the cart.

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Cart = ({ cart, setCart, handleChange }) => {
     const [price, setPrice] = useState(0);
     const [totalItems, setTotalItems] = useState(0);
+
     
     return (
         <div>
